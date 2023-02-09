5a0fba256b549f54fedf854f7a31088570c75139
1234678coc <120152085+1234678coc@users.noreply.github.com>
2023-02-09 11:32:56 +0600

    Add files via upload

---
+++ Новая папка (2)/app.js
@@ -0,0 +1,10 @@
+// console.log("Hello world")
+// const http = require("https");
+// http.createServer(function(request,responce){
+//     response.end("Hello world");
+
+// }).listen(3000,"127.0.0.1",function()){
+//     console.log("Сервер начал прослушивание запросов на порту 3000");
+
+// }
+
---
+++ Новая папка (2)/doc.js
@@ -0,0 +1,33 @@
+// setInterval(function(){
+//     console.log('!');
+// }, 1000)
+// let text = fs.readFileSync('text.txt', 'utf8');
+// console.log(text);
+// fs.writeFileSync('readme.txt', 'text');
+
+
+// a=0;
+// const fs = require('fs')
+// let text = fs.readFileSync('text.txt', 'utf8');
+// setInterval(function(){
+//     fs.writeFileSync('text.txt', text + 'a');
+//          console.log(a++);
+//      },
+// for (let i=0;i<150;i++){
+//     console.log("Hello world");
+// }
+// for (let i=2;i<=20;i++) {
+//     if (i%2===0){
+//         console.log(i);
+//     }
+// }
+// logMassage(5)
+// logMassage(-5)
+// function logMassage(num) {
+//        if(num>0){
+//         console.log('Hello');
+//        }
+//        else {
+//         console.log("olleH");
+//        }
+// }
